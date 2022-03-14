5c91fd9c67639f78c9a6300b56df26b461a9eb12
Qasim Malik <qsmmlk12@gmail.com>
2022-03-14 21:03:55 +0530

    first commit

---
+++ api/category/serializers.py
@@ -0,0 +1,8 @@
+from unicodedata import category
+from rest_framework import serializers
+from .models import Category
+
+class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'description')
---
+++ api/product/serializers.py
@@ -0,0 +1,12 @@
+from unittest.util import _MAX_LENGTH
+from django.forms import ImageField
+from rest_framework import serializers
+from .models import Product
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    image = serializers.ImageField(max_length = None, allow_empty_file = False,
+                                   allow_null = True, required = False)
+    class Meta:
+        model = Product
+        fields = ('id', 'name', 'description', 'price', 'image', 'category')
+        
